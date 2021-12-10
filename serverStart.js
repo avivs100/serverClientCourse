93ff15f7dbdf7bcb0fefa832abd5b8b5ed156d5e
avivs100 <avivshichman@gmail.com>
2021-12-10 15:56:49 +0200

    david fix

--- serverStart.js
+++ serverStart.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var path = require('path');
+var nodemailer = require('nodemailer');
 
 app.use(express.static(__dirname));
 console.log(__dirname);
@@ -23,3 +24,29 @@ app.get('/contactUsPage', function (req, res) {
 
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
+
+
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'avivsagiproj@gmail.com',
+    pass: '123456AS'
+  }
+});
+
+var mailOptions = {
+  from: 'avivsagiproj@gmail.com',
+  to: 'avi1aviv2@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
