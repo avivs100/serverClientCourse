53f5a603ba9aaa0a23c1aef17a64e6b11e32d302
sagifishman <sagifishman1@gmail.com>
2021-12-25 18:46:31 +0200

    ש

--- ForgotPasswordScript.js
+++ ForgotPasswordScript.js
@@ -1,9 +1,9 @@
 //import "sendMailModule.js"
 
 function emailValidation(e){
-
-   // if (recaptchaFunc == true)
-   if(true)
+    var answerFromRecaptcha = recaptchaFunc();
+    console.log(answerFromRecaptcha);
+    if (answerFromRecaptcha == true)
     {
       const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if(!(re.test(String(e).toLowerCase())))
