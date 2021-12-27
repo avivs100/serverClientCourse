02768e7842d267695d8df29d36078a4169bc849d
avivs100 <avivshichman@gmail.com>
2021-12-27 16:42:39 +0200

    back to gmail aviv gay

--- sendMailModule.js
+++ sendMailModule.js
@@ -2,19 +2,19 @@
 
 function sendMailToUser(nodemailer,sentTo,subjectOfMail,textToSend){
     var transporter = nodemailer.createTransport({
-      //   service: 'gmail',
-      //   auth: {
-      //     user: 'avivsagiproj@gmail.com',
-      //     pass: '123456AS'
-      //   }
-      // });
-
-        service: 'hotmail',
+        service: 'gmail',
         auth: {
-          user: process.env.AUTH_EMAIL,
-          pass: process.env.AUTH_PASS,
+          user: 'avivsagiproj@gmail.com',
+          pass: '123456AS'
         }
       });
+
+      //   service: 'hotmail',
+      //   auth: {
+      //     user: process.env.AUTH_EMAIL,
+      //     pass: process.env.AUTH_PASS,
+      //   }
+      // });
       
       var mailOptions = {
         from: 'avivsagiproj@gmail.com',
