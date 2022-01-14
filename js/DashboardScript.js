869751d607c3bdb281d38a929d38d25593d2b390
Tal-Langer <123.tallanger@gmail.com>
2022-01-14 17:36:38 +0200

    n

--- js/DashboardScript.js
+++ js/DashboardScript.js
@@ -1,12 +1,29 @@
 
 
-$.post( '/dashboard' ).done(function (data1) {
-    $(document).ready(function() {
-        $('#example').DataTable( {
-            data : data1
-        } );
-    });
+
+$.post( '/dashboard2' ).done(function (data1) {
+            $(document).ready(function() {
+                $('#example').DataTable( {
+                    data : data1,
+                    columns: [
+                        {data: 'treatmentNum'},
+                        {data: 'treatmentInfo'},
+                        {data: 'date'},
+                        {data: 'workerEmail'},
+                        {data: 'carNum'},
+                        {defaultContent: '<button onclick="deleteRow(123)">Delete</button> / <button onclick="deleteRow(123)">Edit</button>'}
+                    ]
+                } );
+            });
 } );
+
+
+
+
+
+function deleteRow(treatmentNum) {
+    console.log("here");
+}
 
 
 function DashAction(actionNumber)
